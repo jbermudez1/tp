83278565741009d41812f6a6e89cbf52f1a002d9
rociotorres <rocio.torres@proyeccione.com>
2014-10-10 11:23:08 -0600

    Galeria completa - faltan links de video

--- galeria.php
+++ galeria.php
@@ -23,22 +23,278 @@
                 <h2>Galería</h2><span class="rutaProducto"></span>
                 <hr/>
         </div>
-        <span class="rutaProducto">Instalación Panel Top</span><br><br>
+        <span class="rutaProducto">Instalación Panel Top</span><br>
 		<div class="row">
+			<br>
 			<div class="col-xs-3">
 				<a href="img/galeria/top/top1.jpg" rel="shadowbox"><img class="img-responsive" src="img/galeria/top/top1.jpg" /></a>
 			</div>
+			
 			<div class="col-xs-3">
 				<a href="img/galeria/top/top2.jpg" rel="shadowbox"><img class="img-responsive" src="img/galeria/top/top2.jpg" /></a>
 			</div>
+			
 			<div class="col-xs-3">
 				<a href="img/galeria/top/top3.jpg" rel="shadowbox"><img class="img-responsive" src="img/galeria/top/top3.jpg" /></a>
 			</div>
+			
 			<div class="col-xs-3">
 				<a href="img/galeria/top/top4.jpg" rel="shadowbox"><img class="img-responsive" src="img/galeria/top/top4.jpg" /></a>
 			</div>
 		</div>
+
+		<br><br>
+		<span class="rutaProducto">Panel Box</span><br>
+		<div class="row">
+			<br>
+			<div class="col-xs-3">
+				<a href="img/galeria/box25-blanco/01.jpg" rel="shadowbox"><img class="img-responsive" src="img/galeria/box25-blanco/01-1.jpg"/></a>
+			</div>
 			
+			<div class="col-xs-3">
+				<a href="img/galeria/box25-blanco/02.jpg" rel="shadowbox"><img class="img-responsive" src="img/galeria/box25-blanco/02-1.jpg"/></a>
+			</div>
+			
+			<div class="col-xs-3">
+				<a href="img/galeria/box25-blanco/03.jpg" rel="shadowbox"><img class="img-responsive" src="img/galeria/box25-blanco/03-1.jpg"/></a>
+			</div>
+			
+			<div class="col-xs-3">
+				<a href="img/galeria/box25-blanco/04.jpg" rel="shadowbox"><img class="img-responsive" src="img/galeria/box25-blanco/04-1.jpg"/></a>
+			</div>
+			
+			<div class="col-xs-3 margen2">
+				<a href="img/galeria/box25-blanco/05.jpg" rel="shadowbox"><img class="img-responsive" src="img/galeria/box25-blanco/05-1.jpg"/></a>
+			</div>
+			<div class="col-xs-3 margen2">
+				<a href="img/galeria/box25-blanco/06.jpg" rel="shadowbox"><img class="img-responsive" src="img/galeria/box25-blanco/06-1.jpg"/></a>
+			</div>
+			
+			<div class="col-xs-3 margen2">
+				<a href="img/galeria/box25-blanco/07.jpg" rel="shadowbox"><img class="img-responsive" src="img/galeria/box25-blanco/07-1.jpg"/></a>
+			</div>									
+		</div>
+
+		<br><br>
+		<span class="rutaProducto">Instalación de Mec</span><br>
+		<div class="row">
+			<br>
+			<div class="col-xs-3">
+				<a href="img/galeria/mec/mec1.jpg" rel="shadowbox"><img class="img-responsive" src="img/galeria/mec/mec1.jpg" /></a>
+			</div>
+			
+			<div class="col-xs-3">
+				<a href="img/galeria/mec/mec2.jpg" rel="shadowbox"><img class="img-responsive" src="img/galeria/mec/mec2.jpg" /></a>
+			</div>
+			
+			<div class="col-xs-3">
+				<a href="img/galeria/mec/mec3.jpg" rel="shadowbox"><img class="img-responsive" src="img/galeria/mec/mec3.jpg" /></a>
+			</div>
+			
+			<div class="col-xs-3">
+				<a href="img/galeria/mec/mec4.jpg" rel="shadowbox"><img class="img-responsive" src="img/galeria/mec/mec4.jpg" /></a>
+			</div>
+			
+			<div class="col-xs-3 margen2">
+				<a href="img/galeria/mec/mec5.jpg" rel="shadowbox"><img class="img-responsive" src="img/galeria/mec/mec5.jpg" /></a>
+			</div>
+			
+			
+		</div>
+		<br><br>
+		<span class="rutaProducto">Jumbo GSQ</span><br><br>
+		<div class="row">
+			<div class="col-xs-3">
+				<a href="img/galeria/jumbogsq/jumbo1.jpg" rel="shadowbox"><img class="img-responsive" src="img/galeria/jumbogsq/jumbo1.jpg" /></a>
+			</div>
+			
+		</div>
+		<br><br>
+		<span class="rutaProducto">Panel Poliuretano - Comedor Luseme</span><br>
+		<div class="row">
+			<br>
+			<div class="col-xs-3">
+				<a href="img/galeria/panel-poliuretano/luseme1.jpg" rel="shadowbox"><img class="img-responsive" src="img/galeria/panel-poliuretano/luseme1-1.jpg"/></a>
+			</div>
+			
+			<div class="col-xs-3">
+				<a href="img/galeria/panel-poliuretano/luseme2.jpg" rel="shadowbox"><img class="img-responsive" src="img/galeria/panel-poliuretano/luseme2-1.jpg"/></a>
+			</div>
+			
+			<div class="col-xs-3">
+				<a href="img/galeria/panel-poliuretano/luseme3.jpg" rel="shadowbox"><img class="img-responsive" src="img/galeria/panel-poliuretano/luseme3-1.jpg"/></a>
+			</div>
+			
+			<div class="col-xs-3">
+				<a href="img/galeria/panel-poliuretano/luseme4.jpg" rel="shadowbox"><img class="img-responsive" src="img/galeria/panel-poliuretano/luseme4-1.jpg"/></a>
+			</div>
+			
+			<div class="col-xs-3 margen2">
+				<a href="img/galeria/panel-poliuretano/luseme5.jpg" rel="shadowbox"><img class="img-responsive" src="img/galeria/panel-poliuretano/luseme5-1.jpg"/></a>
+			</div>
+			<div class="col-xs-3 margen2">
+				<a href="img/galeria/panel-poliuretano/luseme6.jpg" rel="shadowbox"><img class="img-responsive" src="img/galeria/panel-poliuretano/luseme6-1.jpg"/></a>
+			</div>
+			
+			<div class="col-xs-3 margen2">
+				<a href="img/galeria/panel-poliuretano/luseme7.jpg" rel="shadowbox"><img class="img-responsive" src="img/galeria/panel-poliuretano/luseme7-1.jpg"/></a>
+			</div>
+			
+			<div class="col-xs-3 margen2">
+				<a href="img/galeria/panel-poliuretano/luseme8.jpg" rel="shadowbox"><img class="img-responsive" src="img/galeria/panel-poliuretano/luseme8-1.jpg"/></a>
+			</div>
+
+			<div class="col-xs-3  margen2">
+				<a href="img/galeria/panel-poliuretano/luseme9.jpg" rel="shadowbox"><img class="img-responsive" src="img/galeria/panel-poliuretano/luseme9-1.jpg"/></a>
+			</div>
+			<div class="col-xs-3  margen2">
+				<a href="img/galeria/panel-poliuretano/luseme10.jpg" rel="shadowbox"><img class="img-responsive" src="img/galeria/panel-poliuretano/luseme10-1.jpg"/></a>
+			</div>
+			
+			<div class="col-xs-3 margen2">
+				<a href="img/galeria/panel-poliuretano/luseme11.jpg" rel="shadowbox"><img class="img-responsive" src="img/galeria/panel-poliuretano/luseme11-1.jpg"/></a>
+			</div>
+			
+			<div class="col-xs-3  margen2">
+				<a href="img/galeria/panel-poliuretano/luseme12.jpg" rel="shadowbox"><img class="img-responsive" src="img/galeria/panel-poliuretano/luseme12-1.jpg"/></a>
+			</div>
+
+			<div class="col-xs-3 margen2">
+				<a href="img/galeria/panel-poliuretano/luseme13.jpg" rel="shadowbox"><img class="img-responsive" src="img/galeria/panel-poliuretano/luseme13-1.jpg"/></a>
+			</div>
+			<div class="col-xs-3 margen2">
+				<a href="img/galeria/panel-poliuretano/luseme14.jpg" rel="shadowbox"><img class="img-responsive" src="img/galeria/panel-poliuretano/luseme14-1.jpg"/></a>
+			</div>
+		</div>
+
+			<br><br>
+		<span class="rutaProducto">Panel Imitación Teja</span><br>
+		<div class="row">
+			<br>
+			<div class="col-xs-3">
+				<a href="img/galeria/panel-imitacion-teja/01.jpg" rel="shadowbox"><img class="img-responsive" src="img/galeria/panel-imitacion-teja/01-1.jpg"/></a>
+			</div>
+			
+			<div class="col-xs-3">
+				<a href="img/galeria/panel-imitacion-teja/02.jpg" rel="shadowbox"><img class="img-responsive" src="img/galeria/panel-imitacion-teja/02-1.jpg"/></a>
+			</div>
+			
+			<div class="col-xs-3">
+				<a href="img/galeria/panel-imitacion-teja/03.jpg" rel="shadowbox"><img class="img-responsive" src="img/galeria/panel-imitacion-teja/03-1.jpg"/></a>
+			</div>
+			
+			<div class="col-xs-3">
+				<a href="img/galeria/panel-imitacion-teja/04.jpg" rel="shadowbox"><img class="img-responsive" src="img/galeria/panel-imitacion-teja/04-1.jpg"/></a>
+			</div>
+			
+			<div class="col-xs-3 margen2">
+				<a href="img/galeria/panel-imitacion-teja/05.jpg" rel="shadowbox"><img class="img-responsive" src="img/galeria/panel-imitacion-teja/05-1.jpg"/></a>
+			</div>
+			<div class="col-xs-3 margen2">
+				<a href="img/galeria/panel-imitacion-teja/06.jpg" rel="shadowbox"><img class="img-responsive" src="img/galeria/panel-imitacion-teja/06-1.jpg"/></a>
+			</div>
+			
+			<div class="col-xs-3 margen2">
+				<a href="img/galeria/panel-imitacion-teja/07.jpg" rel="shadowbox"><img class="img-responsive" src="img/galeria/panel-imitacion-teja/07-1.jpg"/></a>
+			</div>
+			
+			<div class="col-xs-3 margen2">
+				<a href="img/galeria/panel-imitacion-teja/08.jpg" rel="shadowbox"><img class="img-responsive" src="img/galeria/panel-imitacion-teja/08-1.jpg"/></a>
+			</div>
+
+			<div class="col-xs-3  margen2">
+				<a href="img/galeria/panel-imitacion-teja/09.jpg" rel="shadowbox"><img class="img-responsive" src="img/galeria/panel-imitacion-teja/09-1.jpg"/></a>
+			</div>
+			<div class="col-xs-3  margen2">
+				<a href="img/galeria/panel-imitacion-teja/10.jpg" rel="shadowbox"><img class="img-responsive" src="img/galeria/panel-imitacion-teja/10-1.jpg"/></a>
+			</div>
+			
+			<div class="col-xs-3 margen2">
+				<a href="img/galeria/panel-imitacion-teja/11.jpg" rel="shadowbox"><img class="img-responsive" src="img/galeria/panel-imitacion-teja/11-1.jpg"/></a>
+			</div>
+			
+			<div class="col-xs-3  margen2">
+				<a href="img/galeria/panel-imitacion-teja/12.jpg" rel="shadowbox"><img class="img-responsive" src="img/galeria/panel-imitacion-teja/12-1.jpg"/></a>
+			</div>
+
+			<div class="col-xs-3 margen2">
+				<a href="img/galeria/panel-imitacion-teja/13.jpg" rel="shadowbox"><img class="img-responsive" src="img/galeria/panel-imitacion-teja/13-1.jpg"/></a>
+			</div>
+			<div class="col-xs-3 margen2">
+				<a href="img/galeria/panel-imitacion-teja/14.jpg" rel="shadowbox"><img class="img-responsive" src="img/galeria/panel-imitacion-teja/14-1.jpg"/></a>
+			</div>
+			<div class="col-xs-3 margen2">
+				<a href="img/galeria/panel-imitacion-teja/15.jpg" rel="shadowbox"><img class="img-responsive" src="img/galeria/panel-imitacion-teja/15-1.jpg"/></a>
+			</div>	
+			
+		</div>
+
+		<br><br>
+		<span class="rutaProducto">Panel Techo</span><br>
+		<div class="row">
+			<br>
+			<div class="col-xs-3">
+				<a href="img/galeria/panel-techo15/01.jpg" rel="shadowbox"><img class="img-responsive" src="img/galeria/panel-techo15/01-1.jpg"/></a>
+			</div>
+			
+			<div class="col-xs-3">
+				<a href="img/galeria/panel-techo15/02.jpg" rel="shadowbox"><img class="img-responsive" src="img/galeria/panel-techo15/02-1.jpg"/></a>
+			</div>
+			
+			<div class="col-xs-3">
+				<a href="img/galeria/panel-techo15/03.jpg" rel="shadowbox"><img class="img-responsive" src="img/galeria/panel-techo15/03-1.jpg"/></a>
+			</div>
+			
+			<div class="col-xs-3">
+				<a href="img/galeria/panel-techo15/04.jpg" rel="shadowbox"><img class="img-responsive" src="img/galeria/panel-techo15/04-1.jpg"/></a>
+			</div>
+			
+			<div class="col-xs-3 margen2">
+				<a href="img/galeria/panel-techo15/05.jpg" rel="shadowbox"><img class="img-responsive" src="img/galeria/panel-techo15/05-1.jpg"/></a>
+			</div>
+			<div class="col-xs-3 margen2">
+				<a href="img/galeria/panel-techo15/06.jpg" rel="shadowbox"><img class="img-responsive" src="img/galeria/panel-techo15/06-1.jpg"/></a>
+			</div>
+			
+			<div class="col-xs-3 margen2">
+				<a href="img/galeria/panel-techo15/07.jpg" rel="shadowbox"><img class="img-responsive" src="img/galeria/panel-techo15/07-1.jpg"/></a>
+			</div>
+			
+			<div class="col-xs-3 margen2">
+				<a href="img/galeria/panel-techo15/08.jpg" rel="shadowbox"><img class="img-responsive" src="img/galeria/panel-techo15/08-1.jpg"/></a>
+			</div>
+
+			<div class="col-xs-3  margen2">
+				<a href="img/galeria/panel-techo15/09.jpg" rel="shadowbox"><img class="img-responsive" src="img/galeria/panel-techo15/09-1.jpg"/></a>
+			</div>
+			<div class="col-xs-3  margen2">
+				<a href="img/galeria/panel-techo15/10.jpg" rel="shadowbox"><img class="img-responsive" src="img/galeria/panel-techo15/10-1.jpg"/></a>
+			</div>
+			
+			<div class="col-xs-3 margen2">
+				<a href="img/galeria/panel-techo15/11.jpg" rel="shadowbox"><img class="img-responsive" src="img/galeria/panel-techo15/11-1.jpg"/></a>
+			</div>
+			
+			<div class="col-xs-3  margen2">
+				<a href="img/galeria/panel-techo15/12.jpg" rel="shadowbox"><img class="img-responsive" src="img/galeria/panel-techo15/12-1.jpg"/></a>
+			</div>
+
+			<div class="col-xs-3 margen2">
+				<a href="img/galeria/panel-techo15/13.jpg" rel="shadowbox"><img class="img-responsive" src="img/galeria/panel-techo15/13-1.jpg"/></a>
+			</div>
+			<div class="col-xs-3 margen2">
+				<a href="img/galeria/panel-techo15/14.jpg" rel="shadowbox"><img class="img-responsive" src="img/galeria/panel-techo15/14-1.jpg"/></a>
+			</div>
+			<div class="col-xs-3 margen2">
+				<a href="img/galeria/panel-techo15/15.jpg" rel="shadowbox"><img class="img-responsive" src="img/galeria/panel-techo15/15-1.jpg"/></a>
+			</div>
+		</div>
+
+
+			
+
+
+
 	</section>
 	<footer>
 		<?php include('footer.php') ?>
